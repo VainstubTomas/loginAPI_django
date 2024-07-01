a2615807e01f0edbe50609da475505ac7beb3ace
Tomas Vainstub <vainstubtomas@gmail.com>
2024-07-01 09:15:19 -0300

    .

--- frontClient/src/apiManagment/axios_test.js
+++ frontClient/src/apiManagment/axios_test.js
@@ -2,12 +2,4 @@ import axios from "axios";
 
 const url = 'http://127.0.0.1:8000/'
 
-const userApi = axios.create({
-    baseURL: `${url}/api/v1/user`,
-});
 
-export const getUsers = () => userApi.get("/");
-export const getuserById = (id) => userApi.get(`/${id}/`);
-export const createuser = (user) => userApi.post("/", user);
-export const updateuser = (id, user) => userApi.put(`/${id}/`, user);
-export const deleteuser = (id) => userApi.delete(`/${id}`);

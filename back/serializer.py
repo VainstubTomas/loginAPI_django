96f5ff06846331e9b3716c9c7b443eae0c9889bf
Tomas Vainstub <vainstubtomas@gmail.com>
2024-07-02 09:51:08 -0300

    Finish API first config

--- back/serializer.py
+++ back/serializer.py
@@ -3,6 +3,6 @@ from .models import User
 
 
 class userSerializer(serializers.ModelSerializer):
-    class meta:
+    class Meta:
         model = User
         fields = "__all__"
--- loginAPI/urls.py
+++ loginAPI/urls.py
@@ -1,11 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
 from back import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('back.urls', namespace='back')),
-    path('api/', include('back.urls', namespace='back_api')),
+    path('api/', include('back.urls', namespace='back')),
+    path('',include('back.urls', namespace='back'))
 ]
